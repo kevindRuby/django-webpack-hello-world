1b74a54aa2861b4eed5af1120760feb6945ad05c
kevindanTT <k.dandrade@live.com>
2024-10-13 14:06:21 +0000

    Run Webpack

---
+++ helloapp/static/helloapp/bundle/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./helloapp/static/helloapp/js/main.js":
+/*!*********************************************!*\
+  !*** ./helloapp/static/helloapp/js/main.js ***!
+  \*********************************************/
+/***/ (() => {
+
+eval("setInterval(() => {\n    const element = document.getElementById('blink');\n    element.style.visibility = (element.style.visibility === 'hidden') ? 'visible' : 'hidden';\n  }, 500);\n  \n\n//# sourceURL=webpack://django-webpack-hello-world/./helloapp/static/helloapp/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./helloapp/static/helloapp/js/main.js"]();
+/******/ 	
+/******/ })()
+;

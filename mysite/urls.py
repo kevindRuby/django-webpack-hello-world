7e8ed2520d5fed068bc99cad77def5ffc04a1531
kevindanTT <k.dandrade@live.com>
2024-10-13 14:08:53 +0000

    For main.html configure mysite/urls.py by adding a URL route

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from helloapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.main_view, name='main'),
 ]
